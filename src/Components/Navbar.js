4018fd96f79542b79d4404ea5118a74b4fcbe43d
Oladapo Daniel OLATUBOSUN <odapo138@gmail.com>
2023-12-05 12:33:46 +0100

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Logo from "../Assets/Logo.svg";
+import { BaCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import {Box, Drawer, ListItem, ListItemButton, ListItemText} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home"; 
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
+const Navbar = () => {
+
+    const [openMenu, setOpenMenu] = useState(false)
+    const menuOptions = [
+        {
+            text: "Home",
+            icon: <HomeIcon/>
+        },
+        {
+            text: "About",
+            icon: <InfoIcon/>
+        },
+        {
+            text: "Testimonials",
+            icon: <CommentRoundedIcon/>
+        },
+        {
+            text: "Contact",
+            icon: <PhoneRoundedIcon/>
+        },
+        {
+            text: "Cart",
+            icon: <ShoppingCartRoundedIcon/>
+        }
+    ]
+
+    return <nav>
+        <div>
+            
+        </div>
+    </nav>
+};
+
+export default Navbar;
